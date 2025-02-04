c249d00533a0c03bb1da095480edf304cea2be6c
kul chandra bhatt <114309808+bhattk64@users.noreply.github.com>
2025-02-04 11:54:32 +0530

    Basic syntax of python

---
+++ helloworld.py
@@ -0,0 +1,17 @@
+#variables
+x=5
+y=4
+print(x)
+print(y)
+#casting 
+print(str(x))
+print(int(y))
+print(float(x))
+print(type(x))
+print("hello")
+a="Hlo"
+print(a)
+A="Hello this is nikhil"
+print(A[0])
+B="Hello this is kulchandr"
+print(B[2:5])
